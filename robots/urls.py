e875cddb8365b818f0dde814609ed0852ccaf6d1
Yermakhan <melanhany@gmail.com>
2023-10-06 05:48:56 +0300

    added openpyxl to dependencies

--- robots/urls.py
+++ robots/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("api/robots/", views.post_robot)
+    path("api/robots/", views.post_robot),
+    path("download_weekly_report/", views.generate_weekly_report),
 ]
